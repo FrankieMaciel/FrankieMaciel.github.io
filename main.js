39ec451abdf515b88164fe212c4d1f613ff361fb
Frankie <110301698+FrankieMaciel@users.noreply.github.com>
2025-10-05 16:05:42 -0300

    adicionando beleza

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-const tully_speak_text = "Oi eu sou tully! e aqui você encontra informações sobre meus desenhos.\n\n Algumas dessas informações consiste em: Exemplo de artes, tabela de preços, Termos de Serviços e links para contato!"
 const artGrid = document.getElementById('art-grid');
-const tullySpeakElement = document.getElementById('tullySpeak');
-const artQuantity = 5;
+const artQuantity = 6;
 
 for (let i = 0; i < artQuantity; i++) {
     var img = document.createElement("img");
@@ -9,14 +7,3 @@ for (let i = 0; i < artQuantity; i++) {
     img.classList.add("art_img");
     artGrid.appendChild(img);
 }
-
-const words = tully_speak_text.split(' ');
-let frase = '';
-for (let i = 0; i < words.length; i++) {
-    setTimeout(function() {
-        frase += words[i] + ' ';
-        tullySpeakElement.innerText = frase;
-      }, 100 * i);
-}
-
-
